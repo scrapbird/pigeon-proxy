b120ed25b441afcb01538965d03f946c3f861512
Brenton Morris <brenton.smorris@gmail.com>
2013-11-04 13:30:23 +1300

    Creating simple proxy rules

--- proxy.js
+++ proxy.js
@@ -1,3 +1,28 @@
-// Simple http proxy using node-http-proxy
+// Simple http routing proxy
 
 // modules
+var http = require('http');
+var httpProxy = require('http-proxy');
+
+// create an instance of the routing proxy
+var proxy = new httpProxy.RoutingProxy();
+
+// get configuration
+
+http.createServer(function (req, res) {
+	var targetHost = 'localhost';
+	var targetPort = 0;
+
+	// decide which server to route to
+	if (req.headers.host.indexOf('devit.co') !== -1) {
+		targetPort = 8081;
+	} else if (req.headers.host.indexOf('pigeon.peerspeak.net') !== -1) {
+		targetPort = 8080;
+	}
+
+	proxy.proxyRequest(req, res, {
+		host: 'localhost',
+		port: targetPort
+	});
+
+}).listen(80);
